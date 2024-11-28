5f9e33f7a64601db32bacd78e1d359add88a5801
hamaguchi21 <k23100kk@aitech.ac.jp>
2024-11-28 15:49:37 +0900

    add K23100Diary

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from diaries.DiarySample import DiarySample
 from diaries.K23100Diary import K23100Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(),
-           K23100Diary(),
-             ] 
+diaries = [DiarySample(), K23100Diary(),] 
 
 for d in diaries:
     print("---------------------------------")
